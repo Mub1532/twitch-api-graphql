b249572e29d61785c0c96408f614ca371ff49186
Mub1532 <mubkhandiscord@gmail.com>
2022-10-26 20:29:42 +0000

    misc

--- test.ts
+++ test.ts
@@ -5,7 +5,7 @@ const client = new TwitchClient("kimne78kx3ncx6brgo4mv6wki5h1ko");
 async function ok(){
     const lmao = await client.fetchUserByID("32787655")
 
-    console.log(lmao)
+    console.log(JSON.stringify(lmao, null,3))
 }
 
 ok()
